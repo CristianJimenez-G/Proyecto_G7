9edcc25e7ed1033026c7f2734bc61e99368e5868
CristianJimenez-G <gasparjim05@gmail.com>
2026-05-29 22:35:45 -0500

    Teminar el Activity Producto_Seleccionado

--- app/src/main/java/com/example/idatdemo/ListaComprasActivity.kt
+++ app/src/main/java/com/example/idatdemo/ListaComprasActivity.kt
@@ -1,16 +1,59 @@
 package com.example.idatdemo
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class ListaComprasActivity : AppCompatActivity() {
+    private lateinit var lvProductos :ListView
+    private lateinit var tietProducto : TextInputEditText
+    private lateinit var btnGuardar : MaterialButton
+    private lateinit var tilProducto: textInputLayaout
+
+    private val produtos = mutableListOf<String>()
+    private lateinit var productosAdapter : ArrayAdapter<String>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_lista_compras)
+
+        lvProductos = findViewById(R.id.lvProductos)
+        tietProducto = findViewById(R.id.tietProducto)
+        btnGuardar = findViewById(R.id.btnGuardar)
+
+
+        val productos = mutableListOf("Arroz", "Azucar", "Avena")
+
+        productosAdapter = ArrayAdapter(
+            this,
+
+            android.R.layout.simple_list_item_1,
+            productos
+        )
+
+        lvProductos.adapter = productosAdapter
+
+        btnGuardar.setOnClickListener {
+            val producto = tietProducto.text.toString().trim()
+            if (producto.isNotEmpty()) {
+                producto.add(producto)
+                productosAdapter.notifyDataSetChanged()
+                tietProducto.text?.clear()
+            } else {
+                Toast.makeText(this, "Ingrese un producto", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/idatdemo/MainActivity.kt
+++ app/src/main/java/com/example/idatdemo/MainActivity.kt
@@ -5,11 +5,16 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Button
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var btnAcceder : MaterialButton
     private  lateinit var tvRegistro : TextView
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,12 +22,19 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        tvRegistro = findViewById(R.id.tvRegistro)
-        tvRegistro.setOnClickListener {
+        val tvRegistro = findViewById<TextView>(R.id.tvRegistro)
+        val btnAcceder = findViewById<Button>(R.id.btnAcceder)
 
-            var intent = Intent(this, RegistroActivity::class.java)
+        tvRegistro.setOnClickListener {
+            val intent = Intent(this, RegistroActivity::class.java)
             startActivity(intent)
         }
+
+        btnAcceder.setOnClickListener {
+            val intent = Intent(this, ListaComprasActivity::class.java)
+            startActivity(intent)
+        }
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
